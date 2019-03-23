5e503df09bd07a0a126bc39fda53abd4d96ce9ac
Carrie Cook <46497432+Carrie-Data@users.noreply.github.com>
2019-03-22 19:56:53 -0600

    Upload HW3 python

---
+++ HW3_Python_Challenge/PyBank/main.py
@@ -0,0 +1,83 @@
+import os
+import csv
+
+# Path to collect data from the Resources folder
+BudgetCSV = os.path.join('.', 'budget_data.csv')
+
+# Read in the CSV file
+with open(BudgetCSV, 'r') as csvfile:
+
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    #Calculate Months
+    header = next(csvreader)
+    data = list(csvreader)
+    months= int(len(data))
+
+    #Set initial value/initialize a few variables
+    total_profit = 0
+    total_change = 0
+    min_profit = int(data[1][1]) -int(data[0][1])
+    max_profit = int(data[1][1]) -int(data[0][1])
+
+    #Calculate total profit
+    for row in data:
+        total_profit += int(row[1]) 
+
+    #Calculate the total change in profit from month to month
+    for i in range(months - 1):
+        top_price = int(data[i][1]) 
+        bottom_price = int(data[i + 1][1])
+        change= bottom_price - top_price
+        total_change += change
+
+        #In the same loop examime to change in profit to find the max and min change.
+        if change < min_profit:
+            min_profit = change
+            month_min_profit = data[i + 1][0]
+
+        if change > max_profit:
+            max_profit = change
+            month_max_profit = data[i + 1][0]
+
+    #Calculate average change
+    average = total_change/(months - 1)
+    average = ("{0:.2f}".format(average))
+  
+    #print calculated values
+    print("Financial Analysis")
+    print("-------------------------")
+    print(f"Total Months: {months}")
+    print(f"Total: ${total_profit}")
+    print(f"Average Change: ${average}")
+    print("Greatest Increase in Profits: " + month_max_profit + "  ($" + str(max_profit) + ")")
+    print("Greatest Decrease in Profits: " + month_min_profit + "  ($" + str(min_profit) + ")")
+    print("-----")
+
+#create text file and write results to it.
+# Set variable for output file
+output_file = os.path.join("Financial_Analysis.txt")
+
+#  Open the output file
+with open(output_file, "w", newline="") as datafile:
+    datafile.write("Financial Analysis\n",)
+    datafile.write("-------------------------\n")
+    datafile.write(f"Total Months: {months}\n")
+    datafile.write(f"Total: ${total_profit}\n")
+    datafile.write(f"Average Change: ${average}\n")
+    datafile.write("Greatest Increase in Profits: " + month_max_profit + "  ($" + str(max_profit) + ")\n")
+    datafile.write("Greatest Decrease in Profits: " + month_min_profit + "  ($" + str(min_profit) + ")\n")
+    datafile.write("-----")
+
+    datafile.close()
+
+
+
+    
+
+    
+
+
+
+
---
+++ HW3_Python_Challenge/PyPoll/main.py
@@ -0,0 +1,95 @@
+import os
+import csv
+
+# Path to collect data from the Resources folder
+PollCSV = os.path.join('.', 'Resources', 'election_data.csv')
+
+# Read in the CSV file
+with open(PollCSV, 'r') as csvfile:
+
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+
+    #Create empty list
+    candidate = []
+
+    #Calculate the number of votes ie rows.
+    Poll_data = list(csvreader)
+    Votes = len(Poll_data)
+
+    #Find all the candidates that where voted for.
+    for i in range(Votes):
+        if Poll_data[i][2] not in candidate:
+            candidate.append(Poll_data[i][2])
+ 
+   
+    vote_count = []
+  
+    #Find how many candidates where voted for
+    for person in candidate:
+        vote_count.append(0)
+
+    num_candidate = len(candidate)
+ 
+    #Find out how many votes each candidate got.
+    for i in range(Votes):
+        for j in range(num_candidate):
+            if Poll_data[i][2] == candidate[j]:
+                vote_count[j] = vote_count[j] + 1
+
+   
+    #Calculate Percentage of votes per candidate
+    percentage = []
+    
+    for candidate_vote in vote_count:
+        percentage.append(candidate_vote/Votes)
+
+    #Find max votes and thereby the winner
+    max_votes = max(vote_count)
+    
+    winner = candidate[vote_count.index(max_votes)]
+
+    # '{percentage:.2%}'.format(percentage)
+    f_percent =[]
+    f_percent = ["{:.3%}".format(percentage[0]),"{:.3%}".format(percentage[1]), "{:.3%}".format(percentage[2]), "{:.3%}".format(percentage[3])]
+
+    #print calculated values
+    print("Election Results")
+    print("-------------------------")
+    print(f"Total Votes: {Votes}")
+    print("-------------------------")
+
+    for x in range(num_candidate):
+        print(candidate[x] +": "+ str(f_percent[x]) +" ("+ str(vote_count[x]) + ")")
+
+    print("-------------------------")  
+    print(f"Winner:  {winner}")
+    print("-------------------------")
+
+#create text file and write results to it.
+# Set variable for output file
+output_file = os.path.join("Election_Results.txt")
+
+#  Open the output file
+with open(output_file, "w", newline="") as datafile:
+    datafile.write("Election Results\n")
+    datafile.write("-------------------------\n")
+    datafile.write(f"Total Votes: {Votes}\n")
+    datafile.write("-------------------------\n")
+
+    for x in range(num_candidate):
+        datafile.write(candidate[x] + ": " + str(f_percent[x]) + " (" + str(vote_count[x]) + ")\n")
+
+    datafile.write("-------------------------\n")
+    datafile.write(f"Winner:  {winner}\n")
+    datafile.write("-------------------------\n")
+
+    datafile.close()
+
+
+
+
+
+
+
